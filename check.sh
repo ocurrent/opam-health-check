5b5f2a79e663a3422f5e89875a733e8a3eb58a1b
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2018-02-21 18:19:38 +0000

    Add some reassuring messages

--- check.sh
+++ check.sh
@@ -16,12 +16,14 @@ echo "RUN opam switch create -y $switch" >> Dockerfile
 echo 'RUN opam install -y opam-depext' >> Dockerfile
 echo 'CMD sudo chown opam:opam /logs && opam list --installable --available --short > /logs/pkgs' >> Dockerfile
 docker build -t base-opam-check-all .
+echo "Getting packages list..."
 docker run -v "$logdir:/logs" base-opam-check-all
 
 for pkg in $(cat "$logdir/pkgs"); do
     cd $(mktemp -d)
     echo 'FROM base-opam-check-all' >> Dockerfile
     echo "RUN opam depext -iyv $pkg" >> Dockerfile
+    echo "Checking $pkg..."
     if docker build --rm --force-rm . > "$logdir/$pkg"; then
         mv "$logdir/$pkg" "$logdir/good/$pkg"
     else
