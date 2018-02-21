82eee424307d119c7131a83e320a6b15d6888992
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2018-02-21 17:54:15 +0000

    Fix the list of packages to check

--- check.sh
+++ check.sh
@@ -5,6 +5,7 @@ switch=4.06.1
 
 mkdir -p "$logdir/good"
 mkdir -p "$logdir/bad"
+touch "$logdir/pkgs"
 
 cd $(mktemp -d)
 echo 'FROM ocaml/opam2:debian-9-ocaml-4.05.0' >> Dockerfile
@@ -12,9 +13,11 @@ echo 'RUN sudo apt-get update' >> Dockerfile
 echo 'RUN git pull origin master && opam update' >> Dockerfile
 echo "RUN opam switch create -y $switch" >> Dockerfile
 echo 'RUN opam install -y opam-depext' >> Dockerfile
+echo 'CMD sudo chown opam:opam /logs && opam list --installable --available --short > /logs/pkgs' >> Dockerfile
 docker build -t base-opam-check-all .
+docker run -v "$logdir:/logs" base-opam-check-all
 
-for pkg in $(opam list --installable --available --short); do
+for pkg in $(cat "$logdir/pkgs"); do
     cd $(mktemp -d)
     echo 'FROM base-opam-check-all' >> Dockerfile
     echo "RUN opam depext -iyv $pkg" >> Dockerfile
