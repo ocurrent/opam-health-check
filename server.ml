440840fd0a3a4a89c5ac404d5b707e37da1189f2
Kate <jp.deplaix@gmail.com>
2018-04-11 17:31:43 +0100

    Explicit returned content type

--- server.ml
+++ server.ml
@@ -1,18 +1,19 @@
 open Containers
 open Lwt.Infix
 
-let serv_file ~logdir file =
+let serv_file ~content_type ~logdir file =
+  let headers = Cohttp.Header.init_with "Content-Type" content_type in
   let fname = Filename.concat logdir file in
-  Cohttp_lwt_unix.Server.respond_file ~fname ()
+  Cohttp_lwt_unix.Server.respond_file ~headers ~fname ()
 
 let callback logdir _conn req _body =
   match Uri.path (Cohttp.Request.uri req) with
-  | "" | "/" -> serv_file ~logdir "index.html"
+  | "" | "/" -> serv_file ~content_type:"text/html" ~logdir "index.html"
   | file ->
       if String.mem ~sub:".." file then
         Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"You bastard !" ()
       else
-        serv_file ~logdir file
+        serv_file ~content_type:"text/plain" ~logdir file
 
 let () =
   match Sys.argv with
