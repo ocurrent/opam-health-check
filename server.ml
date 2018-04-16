086361f1609b52cc4209d9dfa97d493b33b70103
Kate <jp.deplaix@gmail.com>
2018-04-16 16:24:06 +0100

    server: Normalize received paths

--- server.ml
+++ server.ml
@@ -1,19 +1,38 @@
 open Containers
 open Lwt.Infix
 
+module Path : sig
+  type elt
+  type t = elt list
+
+  val of_uri : Uri.t -> t
+  val to_string : t -> string
+end = struct
+  type elt = string
+  type t = elt list
+
+  let rec normalize_path = function
+    | [] -> []
+    | ""::xs -> normalize_path xs
+    | ".."::_ -> failwith "You bastard !"
+    | x::xs -> x :: normalize_path xs
+
+  let of_uri path =
+    normalize_path (String.split_on_char '/' (Uri.path path))
+
+  let to_string path =
+    String.concat "/" path
+end
+
 let serv_file ~content_type ~logdir file =
   let headers = Cohttp.Header.init_with "Content-Type" content_type in
   let fname = Filename.concat logdir file in
   Cohttp_lwt_unix.Server.respond_file ~headers ~fname ()
 
 let callback logdir _conn req _body =
-  match Uri.path (Cohttp.Request.uri req) with
-  | "" | "/" -> serv_file ~content_type:"text/html" ~logdir "index.html"
-  | file ->
-      if String.mem ~sub:".." file then
-        Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"You bastard !" ()
-      else
-        serv_file ~content_type:"text/plain" ~logdir file
+  match Path.of_uri (Cohttp.Request.uri req) with
+  | [] -> serv_file ~content_type:"text/html" ~logdir "index.html"
+  | path -> serv_file ~content_type:"text/plain" ~logdir (Path.to_string path)
 
 let () =
   match Sys.argv with
