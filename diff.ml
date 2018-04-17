214b419b8220dd64ce44a44c89226f882796b137
Kate <jp.deplaix@gmail.com>
2018-04-17 18:10:44 +0100

    server: Simplify the way links to logs are created

--- diff.ml
+++ diff.ml
@@ -6,9 +6,7 @@ type state = Good | Bad
 
 type comp = string
 type pkg = string
-type instance_name = string
-type file = string
-type pkgs = (pkg * (instance_name * (file * state)) list) list
+type pkgs = (pkg * (comp * state) list) list
 
 let get_files dirname =
   let dir = Unix.opendir dirname in
@@ -29,9 +27,9 @@ let get_dirs dir =
   let dirs = List.filter (is_directory dir) files in
   List.sort OpamVersionCompare.compare dirs
 
-let pkg_update ~comp ~file v = function
-  | None -> Some [(comp, (file, v))]
-  | Some l -> Some ((comp, (file, v)) :: l)
+let pkg_update ~comp v = function
+  | None -> Some [(comp, v)]
+  | Some l -> Some ((comp, v) :: l)
 
 let get_pkgs_from_dir ~logdir pkgs comp =
   let dir = Filename.concat logdir comp in
@@ -39,23 +37,23 @@ let get_pkgs_from_dir ~logdir pkgs comp =
   let baddir = Filename.concat dir "bad" in
   let good_files = get_files gooddir in
   let bad_files = get_files baddir in
-  let aux dir v pkgs pkg =
-    Pkgs.update pkg (pkg_update ~comp ~file:(dir^pkg) v) pkgs
+  let aux v pkgs pkg =
+    Pkgs.update pkg (pkg_update ~comp v) pkgs
   in
-  let pkgs = List.fold_left (aux ("/"^comp^"/good/") Good) pkgs good_files in
-  List.fold_left (aux ("/"^comp^"/bad/") Bad) pkgs bad_files
+  let pkgs = List.fold_left (aux Good) pkgs good_files in
+  List.fold_left (aux Bad) pkgs bad_files
 
 let get_pkgs ~logdir compilers =
   let pkgs = List.fold_left (get_pkgs_from_dir ~logdir) Pkgs.empty compilers in
   let pkgs = Pkgs.bindings pkgs in
   List.sort (fun (x, _) (y, _) -> OpamVersionCompare.compare x y) pkgs
 
-let instance_to_html instances comp =
+let instance_to_html ~pkg instances comp =
   let open Tyxml.Html in
   let td c = td ~a:[a_class ["result-col"]; a_style ("background-color: "^c^";")] in
   match List.Assoc.get ~eq:String.equal comp instances with
-  | Some (_path, Good) -> td "green" [pcdata "☑"]
-  | Some (path, Bad) -> td "red" [a ~a:[a_href path] [pcdata "☒"]]
+  | Some Good -> td "green" [pcdata "☑"]
+  | Some Bad -> td "red" [a ~a:[a_href ("/"^comp^"/bad/"^pkg)] [pcdata "☒"]]
   | None -> td "grey" [pcdata "☐"]
 
 let is_pkg_available instances compilers =
@@ -64,7 +62,7 @@ let is_pkg_available instances compilers =
 let pkg_to_html compilers (pkg, instances) =
   let open Tyxml.Html in
   if is_pkg_available instances compilers then
-    Some (tr (td [pcdata pkg] :: List.map (instance_to_html instances) compilers))
+    Some (tr (td [pcdata pkg] :: List.map (instance_to_html ~pkg instances) compilers))
   else
     None
 
