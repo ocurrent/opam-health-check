9eb4302c64d53c36ebe342eab51d850829327f18
Kate <jp.deplaix@gmail.com>
2018-04-23 14:37:33 +0100

    check: Fix and harden log handling

--- check.ml
+++ check.ml
@@ -1,9 +1,6 @@
 open Containers
 open Lwt.Infix
 
-type img = string
-type pkgs = string list
-
 let write_line_unix fd s =
   Lwt_io.write_line (Lwt_io.of_fd ~mode:Lwt_io.Output fd) s
 
@@ -22,73 +19,97 @@ let mkdir_p dir =
 
 let pool = Lwt_pool.create 32 (fun () -> Lwt.return_unit)
 
-let exec_in ~stdin ~stdout ~stderr cmd =
+let proc_fd_of_unix = function
+  | `Close -> `Close
+  | `Dev_null -> `Dev_null
+  | `FD_move fd -> `FD_move (Lwt_unix.unix_file_descr fd)
+  | `FD_copy fd -> `FD_move (Lwt_unix.unix_file_descr fd)
+  | `Keep -> `Keep
+
+exception Process_failure
+
+let exec ~stdin ~stdout ~stderr cmd =
+  let stdin = proc_fd_of_unix stdin in
+  let stdout = proc_fd_of_unix (`FD_copy stdout) in
+  let stderr_lwt = stderr in
+  let stderr = proc_fd_of_unix (`FD_copy stderr) in
   Lwt_process.exec ~stdin ~stdout ~stderr ("", Array.of_list cmd) >>= function
   | Unix.WEXITED 0 ->
-      Lwt.return (Ok ())
+      Lwt.return_unit
   | _ ->
       let cmd = String.concat " " cmd in
-      Lwt_io.write_line Lwt_io.stderr ("Command '"^cmd^"' failed.") >>= fun () ->
-      Lwt.return (Error ())
+      write_line_unix stderr_lwt ("Command '"^cmd^"' failed.") >>= fun () ->
+      Lwt.fail Process_failure
 
-let docker_build ~stdout args dockerfile =
-  let stdout = Lwt_unix.unix_file_descr stdout in
-  let stdin, fd = Lwt_unix.pipe_out () in
+let docker_build ~stderr ~img_name dockerfile =
+  let stdin, fd = Lwt_unix.pipe () in
   let stdin = `FD_move stdin in
-  let stderr = `FD_copy stdout in
-  let stdout = `FD_copy stdout in
-  let fd = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
-  Lwt_io.write_line fd dockerfile >>= fun () ->
-  Lwt_io.close fd >>= fun () ->
-  exec_in ~stdin ~stdout ~stderr ("docker"::"build"::args@["-"])
+  write_line_unix fd dockerfile >>= fun () ->
+  Lwt_unix.close fd >>= fun () ->
+  exec ~stdin ~stdout:stderr ~stderr ["docker";"build";"-t";img_name;"-"]
 
-let docker_run ~stdout img cmd =
-  let stdout = Lwt_unix.unix_file_descr stdout in
-  let stderr = `FD_move stdout in
-  let stdout = `FD_copy stdout in
-  exec_in ~stdin:`Close ~stdout ~stderr ("docker"::"run"::"--rm"::img::cmd)
+let docker_run ~stdout ~stderr img cmd =
+  exec ~stdin:`Close ~stdout ~stderr ("docker"::"run"::"--rm"::img::cmd)
 
-let get_pkgs ~stdout ~dockerfile =
+let get_pkgs ~stderr ~dockerfile =
   let md5 = Digest.to_hex (Digest.string dockerfile) in
   let img_name = "opam-check-all-" ^ md5 in
-  docker_build ~stdout ["-t"; img_name] dockerfile >>= fun _ ->
-  write_line_unix stdout "Getting packages list..." >>= fun () ->
-  Lwt_process.pread ("", [|"docker"; "run"; img_name|]) >|= fun pkgs ->
+  docker_build ~stderr ~img_name dockerfile >>= fun () ->
+  let fd, stdout = Lwt_unix.pipe () in
+  write_line_unix stderr "Getting packages list..." >>= fun () ->
+  docker_run ~stdout ~stderr img_name [] >>= fun () ->
+  Lwt_unix.close stdout >>= fun () ->
+  Lwt_io.read (Lwt_io.of_fd ~mode:Lwt_io.Input fd) >>= fun pkgs ->
+  Lwt_unix.close fd >|= fun () ->
   (img_name, String.split_on_char '\n' pkgs)
 
-(* TODO: Redirect everything to a per user & jobs log *)
-let rec get_jobs ~stdout ~img_name ~logdir ~gooddir ~baddir = function
+let rec get_jobs ~stderr ~img_name ~logdir ~gooddir ~baddir jobs = function
   | [] ->
       Lwt_pool.use pool begin fun () ->
+        Lwt.join jobs >>= fun () ->
         Cache.clear ();
-        Lwt_unix.close stdout
+        Lwt_unix.close stderr
       end
   | pkg::pkgs ->
-      Lwt_pool.use pool begin fun () ->
-        let goodlog = Filename.concat gooddir pkg in
-        let badlog = Filename.concat baddir pkg in
-        Lwt_unix.file_exists goodlog >>= fun goodlog_exists ->
-        Lwt_unix.file_exists badlog >>= fun badlog_exists ->
-        if goodlog_exists || badlog_exists then begin
-          write_line_unix stdout (pkg^" has already been checked. Skipping...")
-        end else begin
-          write_line_unix stdout ("Checking "^pkg^"...") >>= fun () ->
-          let logfile = Filename.concat logdir pkg in
-          Lwt_unix.openfile logfile [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o640 >>= fun stdout ->
-          docker_run ~stdout img_name ["opam";"depext";"-ivy";pkg] >>= begin function
-          | Ok () -> Lwt_unix.rename logfile goodlog
-          | Error () -> Lwt_unix.rename logfile badlog
+      let job =
+        Lwt_pool.use pool begin fun () ->
+          let goodlog = Filename.concat gooddir pkg in
+          let badlog = Filename.concat baddir pkg in
+          Lwt_unix.file_exists goodlog >>= fun goodlog_exists ->
+          Lwt_unix.file_exists badlog >>= fun badlog_exists ->
+          if goodlog_exists || badlog_exists then begin
+            write_line_unix stderr (pkg^" has already been checked. Skipping...")
+          end else begin
+            write_line_unix stderr ("Checking "^pkg^"...") >>= fun () ->
+            let logfile = Filename.concat logdir pkg in
+            Lwt_unix.openfile logfile [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o640 >>= fun stdout ->
+            Lwt.finalize begin fun () ->
+              Lwt.catch begin fun () ->
+                docker_run ~stdout ~stderr:stdout img_name ["opam";"depext";"-ivy";pkg] >>= fun () ->
+                Lwt_unix.rename logfile goodlog
+              end begin function
+              | Process_failure -> Lwt_unix.rename logfile badlog
+              | e -> Lwt.fail e
+              end
+            end begin fun () ->
+              Lwt_unix.close stdout
+            end
           end
         end
-      end |> Lwt.ignore_result;
-      get_jobs ~stdout ~img_name ~logdir ~gooddir ~baddir pkgs
+      in
+      get_jobs ~stderr ~img_name ~logdir ~gooddir ~baddir (job :: jobs) pkgs
 
 let check ~logdir ~dockerfile name =
   let logfile = Filename.concat logdir (name^".log") in
-  Lwt_unix.openfile logfile [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o640 >>= fun stdout ->
-  get_pkgs ~stdout ~dockerfile >>= fun (img_name, pkgs) ->
-  let gooddir = Filename.concat logdir "good" in
-  let baddir = Filename.concat logdir "bad" in
-  mkdir_p gooddir >>= fun () ->
-  mkdir_p baddir >>= fun () ->
-  get_jobs ~stdout ~img_name ~logdir ~gooddir ~baddir pkgs
+  Lwt_unix.openfile logfile [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o640 >>= fun stderr ->
+  Lwt.catch begin fun () ->
+    get_pkgs ~stderr ~dockerfile >>= fun (img_name, pkgs) ->
+    let gooddir = Filename.concat logdir "good" in
+    let baddir = Filename.concat logdir "bad" in
+    mkdir_p gooddir >>= fun () ->
+    mkdir_p baddir >>= fun () ->
+    get_jobs ~stderr ~img_name ~logdir ~gooddir ~baddir [] pkgs
+  end begin fun e ->
+    Lwt_unix.close stderr >>= fun () ->
+    Lwt.fail e
+  end
