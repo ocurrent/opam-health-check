afd65d943c2d4bf3c3820d717cdbfcad51b0feab
Kate <jp.deplaix@gmail.com>
2018-04-23 18:15:41 +0100

    Fix the mkdir -p implementation

--- check.ml
+++ check.ml
@@ -12,7 +12,12 @@ let mkdir_p dir =
         Lwt.return_unit
     | x::xs ->
         let dir = Filename.concat base x in
-        Lwt_unix.mkdir dir 0o750 >>= fun () ->
+        Lwt.catch begin fun () ->
+          Lwt_unix.mkdir dir 0o750
+        end begin function
+        | Unix.Unix_error (Unix.EEXIST, _, _) -> Lwt.return_unit
+        | e -> Lwt.fail e
+        end >>= fun () ->
         aux dir xs
   in
   match String.Split.list_cpy ~by:Filename.dir_sep dir with
