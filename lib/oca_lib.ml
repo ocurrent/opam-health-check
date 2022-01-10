3152bc3878f3a0844839bc6874bcd32f744c1b6a
Antonin Décimo <antonin@tarides.com>
2022-01-10 10:25:47 +0100

    Use lwt_ppx instead of Lwt.finally

--- lib/oca_lib.ml
+++ lib/oca_lib.ml
@@ -124,7 +124,7 @@ let mkdir_p dir =
 
 let rec rm_rf dirname =
   let%lwt dir = Lwt_unix.opendir (Fpath.to_string dirname) in
-  Lwt.finalize begin fun () ->
+  begin
     let rec rm_files () =
       match%lwt Lwt_unix.readdir dir with
       | "." | ".." -> rm_files ()
@@ -140,10 +140,11 @@ let rec rm_rf dirname =
           rm_files ()
     in
     try%lwt rm_files () with End_of_file -> Lwt.return_unit
-  end begin fun () ->
+  end
+  [%lwt.finally
     let%lwt () = Lwt_unix.closedir dir in
     Lwt_unix.rmdir (Fpath.to_string dirname)
-  end
+  ]
 
 type timer = float ref
 
