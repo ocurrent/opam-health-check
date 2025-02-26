abb6cfc8e3e00602196bf13970e9c9f1055b2f7c
Marek Kubica <marek@tarides.com>
2025-02-26 10:48:36 +0100

    Fix automatic build failure

--- server/backend/check.ml
+++ server/backend/check.ml
@@ -311,7 +311,7 @@ fi |} pkg pkg pkg (Server_configfile.platform_distribution conf)
         "opam install ./ --depext-only --with-test --with-doc";
         set_up_workspace ~extra_repos;
         Printf.sprintf {|%s dune pkg lock|} dune_path;
-        Printf.sprintf {|%s dune build --profile=release || echo "opam-health-check: Build failed" && exit 1|} dune_path]]
+        Printf.sprintf {|%s dune build --profile=release || (echo "opam-health-check: Build failed" && exit 1)|} dune_path]]
     )
 
 let run_job ~cap ~conf ~pool ~debug ~stderr ~base_obuilder ~extra_repos ~switch ~num logdir pkg =
